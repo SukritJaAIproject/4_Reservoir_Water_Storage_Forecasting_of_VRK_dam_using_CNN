76a7f0fc15d224d0abc3210ecf2f93640128eacd
Sukrit Jaidee <45908411+SukritJaAIproject@users.noreply.github.com>
2020-08-02 22:36:54 +0700

    Add files via upload

---
+++ model5_yearly/query1.py
@@ -0,0 +1,28 @@
+import MySQLdb
+from db1 import *
+import pandas as pd
+
+def data1(LB):
+    hostname = '10.211.1.25'
+    username = 'root'
+    password = 'opt#565784'
+    database = 'predictive'
+
+    myConnection = MySQLdb.connect(host=hostname, user=username, passwd=password, db=database)
+
+    # a = backlimit()
+    a = backlast()
+    df = pd.read_sql(a, myConnection, params=[LB])
+    # df = pd.read_sql(a, myConnection)
+
+    # def doQuery(conn) :
+    #     # a = backlimit()
+    #     a = backlast()
+    #     df = pd.read_sql(a, conn)
+    #     return df
+
+    # df = doQuery(myConnection)
+
+    myConnection.close()
+    return df
+
